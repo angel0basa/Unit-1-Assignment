f7d3564d8e0e6e6f9c1f8007274beb90d7167df9
Angelo Basa <ambasa@uci.edu>
2020-04-20 02:40:08 -0700

    part 2 complete

---
+++ MovieDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetailsViewController.swift
+//  
+//
+//  Created by Angelo Basa on 4/19/20.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Unit 1 Assignment/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Unit 1 Assignment
+//
+//  Created by Angelo Basa on 4/20/20.
+//  Copyright © 2020 Angelo Basa. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
