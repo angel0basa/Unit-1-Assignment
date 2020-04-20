219d5dcf896d7e649dd72d84500717e824b30c5e
Angelo Basa <ambasa@uci.edu>
2020-04-20 14:53:56 -0700

    Added movie details in the collection view

---
+++ Unit 1 Assignment/MovieDetailsGridViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MovieDetailsGridViewController.swift
+//  Unit 1 Assignment
+//
+//  Created by Angelo Basa on 4/20/20.
+//  Copyright © 2020 Angelo Basa. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsGridViewController: UIViewController {
+    var movie: [String:Any]!
+
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var backDropView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseUrl + posterPath)
+        posterView.af_setImage(withURL: posterURL!)
+        
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backDropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        backDropView.af_setImage(withURL: backDropUrl!)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Unit 1 Assignment/MovieGridViewController.swift
+++ Unit 1 Assignment/MovieGridViewController.swift
@@ -59,16 +59,26 @@ class MovieGridViewController: UIViewController,UICollectionViewDataSource,UICol
     
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.item]
+        
+        let detailsViewController = segue.destination as! MovieDetailsGridViewController
+        detailsViewController.movie = movie
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+        
+        
+        
     }
-    */
-
+    
 
 
 }
